8bd101e5acf5344b195d9a5a506ec32968c0c74f
Rinwu1114 <Rinwu1114@gmail.com>
2025-05-23 23:47:36 -0500

    added moving shapes in background

--- app.js
+++ app.js
@@ -1,37 +1,61 @@
 // template_04h0zbs
-// service_5zwqg3k
-// WSMPI8n1cVi-6L52d
+// service_occ0avl
+// I0SSU61SzLzGShPpQ
 
-function contact(event){
-    event.preventDefault();
-    const loading = document.querySelector(`.modal__overlay--loading`)
-    const success = document.querySelector(`.modal__overlay--success`)
-    loading.classList += ` modal__overlay--visible`
-     emailjs
-     .sendForm(
-        `service_5zwqg3k`,
-        `template_04h0zbs`,
-        event.target,
-        `WSMPI8n1cVi-6L52d`
-    ).then(() =>{
-        loading.classList.remove(`modal__overlay--visible`)
-        success.classList += ` modal__overlay--visible`
-    }).catch(()=>{
-        loading.classList.remove(`modal__overlay--visible`)
-        alert(
-            `The email service is temporarily unavailable. Please contact me directly at Rinwu1114@gmail.com`
-        )
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(`.modal__overlay--loading`);
+  const success = document.querySelector(`.modal__overlay--success`);
+  loading.classList += ` modal__overlay--visible`;
+  emailjs
+    .sendForm(
+      `service_occ0avl`,
+      `template_04h0zbs`,
+      event.target,
+      `I0SSU61SzLzGShPpQ`
+    )
+    .then(() => {
+      loading.classList.remove(`modal__overlay--visible`);
+      success.classList += ` modal__overlay--visible`;
     })
+    .catch(() => {
+      loading.classList.remove(`modal__overlay--visible`);
+      alert(
+        `The email service is temporarily unavailable. Please contact me directly at Rinwu1114@gmail.com`
+      );
+    });
 }
 
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove(`modal--open`);
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
 
-let isModalOpen = false
-function toggleModal(){
-    if (isModalOpen){
-        isModalOpen = false
-        return document.body.classList.remove(`modal--open`)
+let contrastToggle = false;
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += ` dark-theme`;
+  } else {
+    document.body.classList.remove(`dark-theme`);
+  }
+}
+
+const scaleFactor = 1/20
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(`.shape`)
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px,${y * boolInt}px )`
     }
-    isModalOpen = true
-    document.body.classList += " modal--open"
 }
-
